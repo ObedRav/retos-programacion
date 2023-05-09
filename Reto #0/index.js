2893568733620c1f333dd9f833340c2e0a359fdc
Obed Rayo <obedrayo2356@gmail.com>
2023-05-08 20:08:11 -0500

    Añadiendo Reto#0

---
+++ Reto #0/index.js
@@ -0,0 +1,9 @@
+const fizzBuzz = (num) => {
+for (let i = 1; i <= num; i++) {
+    const result = (i % 3 == 0 ? 'fizz' : '') + (i % 5 == 0 ? 'buzz' : '');
+    console.log(result || i);
+    }
+};
+
+// Call the func
+fizzBuzz(100);
