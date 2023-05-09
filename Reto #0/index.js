20c774320a75c3a117eb6738316e7a5f1e313342
Obed Rayo <obedrayo2356@gmail.com>
2023-05-08 20:14:35 -0500

    Semistandard

--- Reto #0/index.js
+++ Reto #0/index.js
@@ -1,8 +1,8 @@
 const fizzBuzz = (num) => {
-for (let i = 1; i <= num; i++) {
-    const result = (i % 3 == 0 ? 'fizz' : '') + (i % 5 == 0 ? 'buzz' : '');
+  for (let i = 1; i <= num; i++) {
+    const result = (i % 3 === 0 ? 'fizz' : '') + (i % 5 === 0 ? 'buzz' : '');
     console.log(result || i);
-    }
+  }
 };
 
 // Call the func
